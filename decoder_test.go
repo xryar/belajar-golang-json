1a254589dc53f0220c9c277f3553463093e16c84
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-04-26 19:09:31 +0700

    streaming decoder

---
+++ decoder_test.go
@@ -0,0 +1,18 @@
+package belajargolangjson
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"testing"
+)
+
+func TestStreamDecoder(t *testing.T) {
+	reader, _ := os.Open("Customer.json")
+	decoder := json.NewDecoder(reader)
+
+	customer := &Customer{}
+	decoder.Decode(customer)
+
+	fmt.Println(customer)
+}
